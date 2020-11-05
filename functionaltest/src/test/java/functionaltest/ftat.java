828cbb30c6129aefdf6e9b66723082529665552e
CVSREDDY <subbareddyvenkata90@gmail.com>
2020-11-05 05:28:52 -0500

    Update ftat.java

--- functionaltest/src/test/java/functionaltest/ftat.java
+++ functionaltest/src/test/java/functionaltest/ftat.java
@@ -16,8 +16,8 @@ public class ftat {
 	public HtmlUnitDriver driver;
 	//public WebDriver driver;
 	public WebDriverWait wait;
-	public String URL = "http://35.196.225.7:8080/QAWebapp/";
-	public String URL1 = "http://35.196.225.7:8080/QAWebapp/avncreatepage.jsp";
+	public String URL = "http://18.223.0.229:8080/AVNCommunication-1.0/";
+	public String URL1 = "http://18.223.0.229:8080/AVNCommunication-1.0/avncreatepage.jsp";
 	//public String URL = "http://localhost:8080/AVNCommunication-1.0/avnlogin.jsp";
 	//public String URL1 = "http://localhost:8080/AVNCommunication-1.0/avncreatepage.jsp";
 	 
